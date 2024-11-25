14d51914bd44e84fc4e4f068582e226d694bbbcf
出口レイ <richard@kanshen.click>
2024-11-25 07:07:51 +0100

    Made some minor adjustments

--- src/components/bg.tsx
+++ src/components/bg.tsx
@@ -9,8 +9,8 @@ function GridBG() {
 function GlowFX() {
     return (
         <div className="fixed inset-0 overflow-hidden pointer-events-none animate-spin">
-            <div className="absolute top-1/2 left-[12.5%] w-[min(25vw,25vh)] h-[min(25vw,25vh)] bg-white/0 rounded-full mix-blend-lighten filter blur-[min(5vw,5vh)] opacity-50 animate-pulse"></div>
-            <div className="absolute bottom-1/2 right-[12.5%] w-[min(25vw,25vh)] h-[min(25vw,25vh)] bg-white/0 rounded-full mix-blend-lighten filter blur-[min(5vw,5vh)] opacity-50 animate-pulse"></div>
+            <div className="absolute top-1/2 left-[12.5%] w-[min(25vw,25vh)] h-[min(25vw,25vh)] bg-white/20 rounded-full mix-blend-lighten filter blur-[min(5vw,5vh)] opacity-50 animate-pulse"></div>
+            <div className="absolute bottom-1/2 right-[12.5%] w-[min(25vw,25vh)] h-[min(25vw,25vh)] bg-white/20 rounded-full mix-blend-lighten filter blur-[min(5vw,5vh)] opacity-50 animate-pulse"></div>
         </div>
     )
 }
