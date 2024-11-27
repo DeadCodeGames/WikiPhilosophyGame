605f9c89c5bd5735d0cc70b553e1b03cde3d3ca2
出口レイ <richard@kanshen.click>
2024-11-27 18:57:04 +0100

    Fixed a thing
    stupidum maximum :3

--- src/utils/wikipediaApi.ts
+++ src/utils/wikipediaApi.ts
@@ -72,8 +72,8 @@ export async function parseWikipediaArticle(
   const nextLink = isRedirect ? redirectTarget : findFirstValidLink(data.parse.text['*'], sectionsToSkip);
 
   return {
-    title: decodeURIComponent(title).split("#")[0],
-    section: decodeURIComponent(title).split("#")[1] || undefined,
+    title: decodeURIComponent(title).replace(/_/g, ' ').split("#")[0],
+    section: decodeURIComponent(title).replace(/_/g, ' ').split("#")[1] || undefined,
     extract,
     nextLink,
     isRedirect,
